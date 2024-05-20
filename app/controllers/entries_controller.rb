14d5ff9d7146d212e06910c3f82d740f871a9e5c
Samarth Saran <samarth.saran@kellogg.northwestern.edu>
2024-05-20 21:59:30 +0000

    final commit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,18 +1,20 @@
 class EntriesController < ApplicationController
   
+#creates new form for places
   def new
     @place = Place.find_by({"id" => params["place_id"]})
   end
   
+#assigns inputs from entries page and creates new array in entry hash 
   def create
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["posted_on"] = params["date"]
+
+   # links entry to place based on ID,saves entry and then redirects to places homepage
     @entry["place_id"] = params["place_id"]
-    
     @entry.save
-  
     redirect_to "/places/#{@entry["place_id"]}"
   end
 end
