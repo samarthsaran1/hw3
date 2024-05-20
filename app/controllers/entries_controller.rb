756fbbf408a5ee55173345c39680dd9a42c347f3
Samarth Saran <samarth.saran@kellogg.northwestern.edu>
2024-05-20 21:46:44 +0000

    almost there

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,18 @@
-class EntrierController < ApplicationController
+class EntriesController < ApplicationController
+  
+  def new
+    @place = Place.find_by({"id" => params["place_id"]})
+  end
+  
+  def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["date"]
+    @entry["place_id"] = params["place_id"]
+    
+    @entry.save
+  
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
 end
